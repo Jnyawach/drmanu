a147e9aa0774814c4098bbc44fc2e4ebefa83cda
Jnyawach <60853597+Jnyawach@users.noreply.github.com>
2022-02-23 18:57:01 +0300

    drop

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -129,15 +129,21 @@
             <div class="row">
                 <div class="col-11 col-md-6 mx-auto">
                     <h2 class="fs-5">Dr. Manu</h2>
-                    <h1 class="fs-2">Integrity and accuracy of Information Matter to Us</h1>
-                    <p>At Healthline, we set high standards of quality, research, and transparency
-                        for what we share, ensuring you have access to nothing but the best. Here's how:</p>
+                    <h1 class="fs-2">We value the accuracy and integrity of information
+                    </h1>
+                    <p>We are dedicated to being your companion in this journey of life.
+                        In doing so, we set exceptional standards for our services and products
+                        and ensure that what we offer is the best. This is how we do it:</p>
                     <ul>
-                        <li>To ensure accuracy, each of our 20,000+ articles is reviewed by a medical subject
-                            matter expert such as a doctor, nurse, or therapist.</li>
-                        <li>Our recommendations are current and based on research thanks to our diligent
-                            health and medical monitoring standards</li>
-                        <li>Featured brands are reviewed for medical credibility, business practices, and social impact.</li>
+                        <li>
+                            We go the extra mile to ensure that each of our articles is reviewed by a qualified
+                            professional to assess the accuracy and get approvals.
+                        </li>
+                        <li>We review our partner brands for credibility and ensure
+                            that they conform to high standards and guidelines</li>
+                        <li>We conduct extensive research on our recommendations
+                            and tips to ensure that they are up to date and based on current medical trends
+                        </li>
                     </ul>
                 </div>
                 <div class="col-11 col-md-6 mx-auto">
